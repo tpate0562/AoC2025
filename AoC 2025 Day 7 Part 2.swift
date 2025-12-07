d5d64e441a93943874e37b3d1096862e92ae47d1
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2025-12-06 22:33:31 -0800

    Create AoC 2025 Day 7 Part 2.swift

---
+++ AoC 2025 Day 7 Part 2.swift
@@ -0,0 +1,33 @@
+import Foundation
+let inputFilePath = "/Users/tejaspatel/Desktop/Programming Projects/AoC 2025 Day 7/AoC 2025 Day 7/input.txt"
+guard let fileContent = readInputFile(at: inputFilePath) else {fatalError()}
+func readInputFile(at path: String) -> String? {
+    do {
+        let content = try String(contentsOfFile: path)
+        return content
+    } catch {
+        return nil
+    }
+}
+var cols = 141, rows = 141, counts: [Int] = Array(repeating: 0, count: cols)
+var statuses: [Bool] = Array(repeating: false, count: rows)
+var array: [[Substring]] = Array(repeating: Array(repeating: "", count: cols), count: rows)
+var rowsData = fileContent.split(separator: "\n")
+for (i, rowData) in rowsData.enumerated() {
+    let chars: [Substring] = rowData.map { String($0)[...] }
+    for (j, ch) in chars.enumerated() {
+        if i < rows && j < cols {
+            array[i][j] = ch
+        }
+    }
+}
+statuses[70] = true
+counts[70]=1
+for i in 1..<cols {
+    for j in 0..<rows {
+        if statuses[j] == true && array[i][j] == "^"{
+            statuses[j] = false; statuses[j-1] = true; statuses[j+1] = true; counts[j-1] += counts[j]; counts[j+1]+=counts[j]; counts[j]=0; print(i,j)
+        }
+    }
+}
+print(counts.reduce(0, +))
